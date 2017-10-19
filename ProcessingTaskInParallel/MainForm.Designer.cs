9733e1d13fdf7562df0c215dd318bf6e56840f51
youngmtool <youngmtool@gmail.com>
2017-10-19 16:20:28 +0900

    f

--- ProcessingTaskInParallel/MainForm.Designer.cs
+++ ProcessingTaskInParallel/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ProcessingTaskInParallel
+﻿namespace DataParallelismWithForEach
 {
     partial class MainForm
     {
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnProcessImages = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnProcessImages
+            // 
+            this.btnProcessImages.Location = new System.Drawing.Point(37, 405);
+            this.btnProcessImages.Name = "btnProcessImages";
+            this.btnProcessImages.Size = new System.Drawing.Size(116, 23);
+            this.btnProcessImages.TabIndex = 0;
+            this.btnProcessImages.Text = "Parallel Process";
+            this.btnProcessImages.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(173, 405);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(123, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel Process";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 369);
+            this.textBox1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(328, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnProcessImages);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnProcessImages;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
-
--- ProcessingTaskInParallel/MainForm.cs
+++ ProcessingTaskInParallel/MainForm.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ProcessingTaskInParallel
+namespace DataParallelismWithForEach
 {
     public partial class MainForm : Form
     {
@@ -16,5 +18,79 @@ namespace ProcessingTaskInParallel
         {
             InitializeComponent();
         }
+
+        // New Form-level variable.
+        private CancellationTokenSource cancelToken = new CancellationTokenSource();
+
+        private void btnProcessImages_Click(object sender, EventArgs e)
+        {
+            // Start a new "task" to process the files.
+            Task.Factory.StartNew(() =>
+            {
+                ProcessFiles();
+            });
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // This will be used to tell all the worker threads to stop!
+            cancelToken.Cancel();
+        }
+
+
+
+
+
+        private void ProcessFiles()
+        {
+            // Use ParallelOptions instance to store the CancellationToken.
+            ParallelOptions parOpts = new ParallelOptions();
+            parOpts.CancellationToken = cancelToken.Token;
+            parOpts.MaxDegreeOfParallelism = System.Environment.ProcessorCount;
+
+            // Load up all *.jpg files, and make a new folder for the modified data.
+            string[] files = Directory.GetFiles
+                (@"C:\PicturesWillbeRotated", "*.jpg",
+                  SearchOption.AllDirectories);
+            string newDir = @"C:\PicturesHaveBeenRotated";
+            Directory.CreateDirectory(newDir);
+            try
+            {
+                // Process the image data in a parallel manner!
+                Parallel.ForEach(files, parOpts, currentFile =>
+                {
+                    parOpts.CancellationToken.ThrowIfCancellationRequested();
+
+                    string filename = Path.GetFileName(currentFile);
+                    using (Bitmap bitmap = new Bitmap(currentFile))
+                    {
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(newDir, filename));
+                        this.Invoke((Action)delegate
+                        {
+                            textBox1.Text  /*this.Text*/ = string.Format("Processing {0} on thread {1}", filename,
+                              Thread.CurrentThread.ManagedThreadId);
+                        }
+                        );
+                    }
+                }
+                );
+                this.Invoke((Action)delegate
+                {
+                    textBox1.Text  /*this.Text*/ = "Done!";
+                });
+            }
+            catch (OperationCanceledException ex)
+            {
+                this.Invoke((Action)delegate
+                {
+                    this.Text = ex.Message;
+                });
+            }
+        }
+
+
+
+
     }
 }
