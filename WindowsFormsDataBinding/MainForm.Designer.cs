688160e800772305027ccb9a5609d43889f9a14b
youngmtool <youngmtool@gmail.com>
2017-10-22 18:57:39 +0900

    Added a winforms project to bind data from DataTable to DataGridView control

---
+++ WindowsFormsDataBinding/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsDataBinding
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carInventoryGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.carInventoryGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // carInventoryGridView
+            // 
+            this.carInventoryGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.carInventoryGridView.Location = new System.Drawing.Point(12, 64);
+            this.carInventoryGridView.Name = "carInventoryGridView";
+            this.carInventoryGridView.RowTemplate.Height = 23;
+            this.carInventoryGridView.Size = new System.Drawing.Size(558, 348);
+            this.carInventoryGridView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(25, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Here is what we have in stock";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 424);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.carInventoryGridView);
+            this.Name = "MainForm";
+            this.Text = "Windows Forms Data Binding";
+            ((System.ComponentModel.ISupportInitialize)(this.carInventoryGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView carInventoryGridView;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsDataBinding/MainForm.cs
@@ -0,0 +1,96 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//Added a winforms project to bind data from DataTable to DataGridView control
+namespace WindowsFormsDataBinding
+{
+
+
+    public partial class MainForm : Form
+    {
+        //Declare listCars field(=global variable, member variable) which will contains Car objects. 
+        List<Car> listCars = null;
+
+
+        //Get a DataTable object, and assign it to inventoryTable field.
+        DataTable inventoryTable = new DataTable();
+
+
+
+
+
+
+        //Add a helper method.
+        void CreateDataTable()
+        {
+            // Create table schema by getting DataColumn object by instantiating it with passing values.
+            //I don't need to set Id record, if I set Id column as a primary key and auto-incrementing behavior
+            var carIDColumn = new DataColumn("Id", typeof(int));
+            var carMakeColumn = new DataColumn("Make", typeof(string));
+            var carColorColumn = new DataColumn("Color", typeof(string));
+            var carPetNameColumn = new DataColumn("PetName", typeof(string))
+            { Caption = "Pet Name" };
+
+            //Add columns which are set above code into inventoryTable DataTable,
+            //passing in an anonymous array which contains each column object
+            inventoryTable.Columns.AddRange(
+              new[] { carIDColumn, carMakeColumn, carColorColumn, carPetNameColumn });
+
+
+            //Iterate over the List<Car> and make rows.
+            //I can use foreach functionality because listCar is generic List type,
+            //so that it implements IEnumerable.
+            foreach (var c in listCars)
+            {
+                //Generated one row example with column.
+                //Id = 1, PetName = "Chucky", Make = "BMW", Color = "Green" 
+                var newRow = inventoryTable.NewRow();
+                newRow["Id"] = c.Id;
+                newRow["Make"] = c.Make;
+                newRow["Color"] = c.Color;
+                newRow["PetName"] = c.PetName;
+                inventoryTable.Rows.Add(newRow);
+            }
+            //With those code, finished to set DataTable, column&schema, row&data
+
+
+            // Bind the inventoryTable DataTable to the DataSource of carInventoryGridView control.
+            carInventoryGridView.DataSource = inventoryTable;
+        }
+
+
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Fill the List with car objects, and then I assign it to the listCars.
+            listCars = new List<Car>
+            {
+                new Car { Id = 1, PetName = "Chucky", Make = "BMW", Color = "Green" },
+                new Car { Id = 2, PetName = "Tiny", Make = "Yugo", Color = "White" },
+                new Car { Id = 3, PetName = "Ami", Make = "Jeep", Color = "Tan" },
+                new Car { Id = 4, PetName = "Pain Inducer", Make = "Caravan", Color = "Pink" },
+                new Car { Id = 5, PetName = "Fred", Make = "BMW", Color = "Green" },
+                new Car { Id = 6, PetName = "Sidd", Make = "BMW", Color = "Black" },
+                new Car { Id = 7, PetName = "Mel", Make = "Firebird", Color = "Red" },
+                new Car { Id = 8, PetName = "Sarah", Make = "Colt", Color = "Black" }
+            };
+
+            CreateDataTable();
+        }
+    }
+}
+
