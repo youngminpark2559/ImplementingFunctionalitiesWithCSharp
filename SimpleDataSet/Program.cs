fe439ecd77244ae3dcc4ac3215ccd47b048a2c9a
youngmtool <youngmtool@gmail.com>
2017-10-22 13:57:22 +0900

    Added a method to inspect RowState of Temp DataTable

--- SimpleDataSet/Program.cs
+++ SimpleDataSet/Program.cs
@@ -44,6 +44,37 @@ namespace SimpleDataSet
         }
 
 
+        //Method to inspect RowState of Temp DataTable
+        private static void ManipulateDataRowState()
+        {
+            // Create a temp DataTable for testing in memory
+            var temp = new DataTable("Temp");
+            temp.Columns.Add(new DataColumn("TempColumn", typeof(int)));
+
+            // RowState = Detached.
+            var row = temp.NewRow();
+            WriteLine($"After calling NewRow(): {row.RowState}");
+
+            // RowState = Added.
+            temp.Rows.Add(row);
+            WriteLine($"After calling Rows.Add(): {row.RowState}");
+
+            // RowState = Added.
+            row["TempColumn"] = 10;
+            WriteLine($"After first assignment: {row.RowState}");
+
+            // RowState = Unchanged.
+            temp.AcceptChanges();
+            WriteLine($"After calling AcceptChanges: {row.RowState}");
+
+            // RowState = Modified.
+            row["TempColumn"] = 11;
+            WriteLine($"After first assignment: {row.RowState}");
+
+            // RowState = Deleted.
+            temp.Rows[0].Delete();
+            WriteLine($"After calling Delete: {row.RowState}");
+        }
 
         static void Main(string[] args)
         {
