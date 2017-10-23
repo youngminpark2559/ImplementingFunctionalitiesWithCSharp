434b37c13278429615f900b3e09711346649f5ae
youngmtool <youngmtool@gmail.com>
2017-10-23 19:06:07 +0900

    Added a partial class Car to override ToString() to define special functionality, in this case for string format and value for null in CarNickName

---
+++ 23/AutoLotConsoleApp/EF/CarPartial.cs
@@ -0,0 +1,16 @@
+﻿namespace AutoLotConsoleApp.EF
+{
+    //Added a partial class Car to override ToString() to define special functionality, in this case for string format and value for null in CarNickName.
+    public partial class Car
+    {
+        //Overrides Car.ToString() to define special functionality, in this case for string format and value for null in CarNickName.
+        public override string ToString()
+        {
+            //Since the PetName column could be empty, 
+            //supply the default name of **No Name**.
+            //**No Name** is a Blue BMW with ID3
+            //Bim is a Green BMW with ID4
+            return $"{this.CarNickName ?? "**No Name**"} is a {this.Color} {this.Make} with ID { this.CarId}.";
+        }
+    }
+}
