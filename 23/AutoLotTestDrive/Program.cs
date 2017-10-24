8d63de0814e15e467815440ed765d3c6cd1c54e8
youngmtool <youngmtool@gmail.com>
2017-10-24 14:29:49 +0900

    Added a helper AddNewRecord(Inventory car) to insert new records, using disconnected layer(InventoryReop -> (IRepo) -> BaseRepo -> my context DbSet<Inventory> in memory) -> connected layer(DbContext -> Database)

--- 23/AutoLotTestDrive/Program.cs
+++ 23/AutoLotTestDrive/Program.cs
@@ -17,7 +17,12 @@ namespace AutoLotTestDrive
         {
             Database.SetInitializer(new DataInitializer());
             WriteLine("***** Fun with ADO.NET EF Code First *****\n");
-            ReadLine();
+            var car1 = new Inventory() { Make = "Yugo", Color = "Brown", PetName = "Brownie" };
+            var car2 = new Inventory() { Make = "SmartCar", Color = "Brown", PetName = "Shorty" };
+            AddNewRecord(car1);
+            AddNewRecord(car2);
+            AddNewRecords(new List<Inventory> { car1, car2 });
+            PrintAllInventory();
         }
 
 
@@ -30,6 +35,31 @@ namespace AutoLotTestDrive
                 {
                     WriteLine(c);
                 }
+            }
+        }
+
+
+
+
+
+
+        //c Added a helper AddNewRecord(Inventory car) to insert new records, using disconnected layer(InventoryReop -> (IRepo) -> BaseRepo -> my context DbSet<Inventory> in memory) -> connected layer(DbContext -> Database)
+        // Add record to the Inventory table of the AutoLot database.
+        private static void AddNewRecord(Inventory car)
+        {
+            //First, add car object passed in by user by invoking Add() in BaseRepo,
+            //and BaseRepo inserts car object into DbSet<Inventory> property storing values in memory,
+            //and finally, within this scope of method, it calls SaveChanges() of DbContext which will do the database job. 
+            using (var repo = new InventoryRepo())
+            {
+                repo.Add(car);
+            }
+        }
+        private static void AddNewRecords(IList<Inventory> cars)
+        {
+            using (var repo = new InventoryRepo())
+            {
+                repo.AddRange(cars);
             }
         }
     }
