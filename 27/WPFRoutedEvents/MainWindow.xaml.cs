7271ed8f90d0529ff8131ef6a7a5aba881635619
youngmtool <youngmtool@gmail.com>
2017-10-26 21:39:51 +0900

    Add code for routed tunneling event which goes down to fire event as opposed to routed bubbling event. When both exists, tunneling event is fired, tunneling and fire event, bubbling event is fired, bubbling and fire event

--- 27/WPFRoutedEvents/MainWindow.xaml.cs
+++ 27/WPFRoutedEvents/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ using System.Windows.Shapes;
 
 //c Add code to stop routed bubbling event from MouseDown event of outerEllipse, so that I can only see title change, not click event for button.
 
+//c Add code for routed tunneling event which goes down to fire event as opposed to routed bubbling event. When both exists, tunneling event is fired, tunneling and fire event, bubbling event is fired, bubbling and fire event.
+
 namespace WPFRoutedEvents
 {
     /// <summary>
@@ -26,6 +28,8 @@ namespace WPFRoutedEvents
     /// </summary>
     public partial class MainWindow : Window
     {
+        string _mouseActivity = string.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,18 +37,43 @@ namespace WPFRoutedEvents
 
         public void btnClickMe_Clicked(object sender, RoutedEventArgs e)
         {
-            // Do something when button is clicked.
-            MessageBox.Show("Clicked the button");
+            AddEventInfo(sender, e);
+            MessageBox.Show(_mouseActivity, "Your Event Info");
+
+            // Clear string for next round.
+            _mouseActivity = "";
         }
 
-        //Click outter one -> Event fired and change title -> Event bubbled up to Button element's Click event 
-        public void outerEllipse_MouseDown(object sender, MouseButtonEventArgs e)
+        private void AddEventInfo(object sender, RoutedEventArgs e)
         {
-            // Change title of window.
-            this.Title = "You clicked the outer ellipse!";
+            _mouseActivity += string.Format(
+              "{0} sent a {1} event named {2}.\n", sender,
+              e.RoutedEvent.RoutingStrategy,
+              e.RoutedEvent.Name);
+        }
 
-            // Stop bubbling!
-            e.Handled = true;
+
+        ////Click outter one -> Event fired and change title -> Event bubbled up to Button element's Click event 
+        //public void outerEllipse_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    // Change title of window.
+        //    this.Title = "You clicked the outer ellipse!";
+
+        //    // Stop bubbling!
+        //    e.Handled = true;
+        //}
+
+
+        //Test for routed bubbling event and routed tunneling event.
+        //1.PreviewMouseDown fired -> no down element for routed tunneling event -> 2.MouseDown fired -> routed bubbling event up to Click event of Button.
+        private void outerEllipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            AddEventInfo(sender, e);
+        }
+
+        private void outerEllipse_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            AddEventInfo(sender, e);
         }
     }
 }
