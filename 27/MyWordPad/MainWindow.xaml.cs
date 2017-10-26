5c85e99c62211533c9769806cefbe35771ca65d3
youngmtool <youngmtool@gmail.com>
2017-10-26 15:16:47 +0900

    Add a WPF project MyWordPad for testing nested panels to build a window's frame

---
+++ 27/MyWordPad/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//c Add a WPF project MyWordPad for testing nested panels to build a window's frame.
+
+namespace MyWordPad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
