c286bad72e85508bc373d4d70c3e1b472c30e7e1
youngmtool <youngmtool@gmail.com>
2017-10-27 19:25:55 +0900

    c Add <List>, <Paragraph> in <FlowDocument> in xaml to implement UI , and add PopulateDocument() to add data

--- 27/WpfControlsAndAPIs/MainWindow.xaml.cs
+++ 27/WpfControlsAndAPIs/MainWindow.xaml.cs
@@ -33,6 +33,8 @@ using System.Windows.Shapes;
 
 //c Update funtionality for document editor which is in the 2nd TabItem by using FlowDocumentReader class and its lower members such as FlowDocument, Section, List, Paragraph..
 
+//c Add <List>, <Paragraph> in <FlowDocument> in xaml to implement UI , and add PopulateDocument() to add data.
+
 namespace WpfControlsAndAPIs
 {
     /// <summary>
@@ -52,6 +54,8 @@ namespace WpfControlsAndAPIs
             this.myInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
             this.inkRadio.IsChecked = true;
             this.comboColors.SelectedIndex = 0;
+
+            PopulateDocument();
         }
 
 
@@ -134,5 +138,45 @@ namespace WpfControlsAndAPIs
             // Clear all strokes.
             this.myInkCanvas.Strokes.Clear();
         }
+
+        private void PopulateDocument()
+        {
+            // Add some data to the List item.
+            this.listOfFunFacts.FontSize = 14;
+            this.listOfFunFacts.MarkerStyle = TextMarkerStyle.Box;
+            this.listOfFunFacts.ListItems.Add(new ListItem(new
+              Paragraph(new Run("Fixed documents are for WYSIWYG print ready docs!"))));
+            this.listOfFunFacts.ListItems.Add(new ListItem(
+    new Paragraph(new Run("The API supports tables and embedded figures!"))));
+            this.listOfFunFacts.ListItems.Add(new ListItem(
+              new Paragraph(new Run("Flow documents are read only!"))));
+            this.listOfFunFacts.ListItems.Add(new ListItem(new Paragraph(new Run
+              ("BlockUIContainer allows you to embed WPF controls in the document!")
+              )));
+
+            // Now add some data to the Paragraph.
+            // First part of sentence.
+            Run prefix = new Run("This paragraph was generated ");
+
+            // Middle of paragraph.
+            Bold b = new Bold();
+            Run infix = new Run("dynamically");
+            infix.Foreground = Brushes.Red;
+            infix.FontSize = 30;
+            b.Inlines.Add(infix);
+
+            // Last part of paragraph.
+            Run suffix = new Run(" at runtime!");
+
+            // Now add each piece to the collection of inline elements
+            // of the Paragraph.
+            this.paraBodyText.Inlines.Add(prefix);
+            this.paraBodyText.Inlines.Add(infix);
+            this.paraBodyText.Inlines.Add(suffix);
+        }
+
+
+
+
     }
 }
